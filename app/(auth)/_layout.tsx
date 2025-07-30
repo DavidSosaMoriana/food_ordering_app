1b213dfe02eddd2cdc20bb6454e92c9add20e90b
DSosa <sosamorianadav@gmail.com>
2025-07-30 20:30:26 +0100

    feat(auth): set up auth screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,53 @@
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { images } from "@/constants";
 import { Slot } from "expo-router";
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 export default function _Layout() {
   return (
-    <SafeAreaView>
-      <Text>Auth layout</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="absolute self-center size-48 -bottom-16 z-10"
+            resizeMode="contain"
+          />
+        </View>
+        <CustomInput
+          placeholder="Enter your email"
+          value={""}
+          onChangeText={(text) => {}}
+          label="Email"
+          secureTextEntry={false}
+          keyboardType="email-address"
+        />
+        <CustomButton />
+      </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.dsm.foodordering",
+    project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "688a5ef600270c9b845b",
+    userCollectionId: "688a60640033e319f896", 
+}
